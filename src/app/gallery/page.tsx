64b9372d2ff1d99f36b8070b1e15910fea5a9edf
Akshay Jain <jainakshay1783@gmail.com>
2025-04-21 23:46:28 -0700

    Create /gallery page with Gallery component

---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,11 @@
+import Gallery from "./Gallery";
+
+const GalleryPage = () => {
+  return (
+    <div className="p-4 h-screen">
+      <Gallery />
+    </div>
+  );
+};
+
+export default GalleryPage;
